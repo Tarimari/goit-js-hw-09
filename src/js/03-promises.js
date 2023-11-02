71e24c2d80fadcb6440cb5fe6110deaeeac84843
Tarimari <tarashorban1987@gmail.com>
2023-11-03 01:49:53 +0200

    3task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+const form = document.querySelector(`.form`);
+
+form.addEventListener(`submit`, startGeneration);
+
+function startGeneration(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget.elements;
+  for (const i = 1; i <= amount; i++) {
+    setTimeout(() => { createPromise(i, delay) }, delay + (i - 1) * step)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  }
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise(resolve, reject)
+  {
+    if (shouldResolve) {
+      resolve;
   } else {
-    // Reject
+      reject;
+    }
   }
 }
