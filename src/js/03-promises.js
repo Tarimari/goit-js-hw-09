3e253d185421ec2adeb4228107eee544641a3c67
Tarimari <tarashorban1987@gmail.com>
2023-11-04 01:22:02 +0200

    3taskFin

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,27 +4,33 @@ form.addEventListener(`submit`, startGeneration);
 
 function startGeneration(evt) {
   evt.preventDefault();
-  const { delay, step, amount } = evt.currentTarget.elements;
-  for (const i = 1; i <= amount; i++) {
-    setTimeout(() => { createPromise(i, delay) }, delay + (i - 1) * step)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  let { delay, step, amount } = evt.currentTarget.elements;
+  for (let position = 1; position <= amount.valueAsNumber; position++) {
+    delay = position * step.valueAsNumber;
+    createPromise(position, delay)
   }
 }
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  return new Promise(resolve, reject)
-  {
-    if (shouldResolve) {
-      resolve;
-  } else {
-      reject;
-    }
-  }
+  setTimeout(() => {
+    new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    })
+  
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+  },
+    delay
+  );
+  
 }
