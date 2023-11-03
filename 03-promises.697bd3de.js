7c07b759623e5d3a370fe5ed9fd794f27dddac75
Tarimari <119706474+Tarimari@users.noreply.github.com>
2023-11-03 23:19:04 +0000

    Deploying to gh-pages from @ Tarimari/goit-js-hw-09@3e253d185421ec2adeb4228107eee544641a3c67 🚀

--- 03-promises.697bd3de.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){var n=function(e){setTimeout((function(){Math.random(),new Promise(resolve,reject)}),t+(e-1)*c).then((function(e){var n=e.position,o=e.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(o,"ms"))})).catch((function(e){var n=e.position,o=e.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(o,"ms"))}))};e.preventDefault();for(var o=e.currentTarget.elements,t=o.delay,c=o.step,r=o.amount,a=1;a<=r;a++)n(a)}));
-//# sourceMappingURL=03-promises.697bd3de.js.map
